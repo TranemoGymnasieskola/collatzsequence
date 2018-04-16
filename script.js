2199a47aa6cda19a8ea6e082194e96823a9a328a
Lars Gunther <gunther@keryx.se>
2018-04-16 14:42:10 +0200

    Fixed empty array, code cleanup

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
+/* eslint-disable no-console */
+
 let terms = 0;
 let high  = 0;
 let highN = undefined;
 let sq    = [];
 
-for (n=1000000; n>1; n--) {
+for (let n=1000000; n>500000; n--) {
+	let candidate = n;
 	while (n !== 1) {
 		if (n % 2 === 0) {
-			n /= 2
+			n /= 2;
 		} else {
-			n *= 3
-			n++
+			n *= 3;
+			n++;
 		}
-		terms++	
+		terms++;
 	} 
 	if (terms > high) {
-		high = terms
-		highN = n
+		high = terms;
+		highN = candidate;
 	}
 }
 
-console.log('the longest sequece with a starting number of 1,000,000 was ' + high + ' terms long with the sequence:')
+console.log('the longest sequece with a starting number of 1,000,000 was ' + high + ' terms long with the sequence:');
 
 while (highN !== 1) {
 	sq.push(highN);
 	if (highN % 2 === 0) {
-		highN /= 2
+		highN /= 2;
 	} else {
-		highN *= 3
-		highN++
+		highN *= 3;
+		highN++;
 	}
 } 
 console.log(sq);
